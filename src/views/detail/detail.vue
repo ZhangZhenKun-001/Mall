79726aa0614bbac346353e1c7d1a4667dcc3fc2b
ZZK <1042134104@qq.com>
2021-01-16 16:40:58 +0800

    详情页

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -1,26 +1,48 @@
 <template>
-  <div>
-    <detail-nav-bar />
-    <detail-swiper :topImages="topImages" />
+  <div id="detail">
+    <detail-nav-bar class="detail-nav" />
+    <scroll class="detail-content" ref="scroll">
+      <detail-swiper :top-images="topImages" />
+      <detail-base-info :goods="goods" />
+      <detail-shop-info :shop="shop" />
+      <detail-goods-info :detail-info="detailInfo" 
+      @imageLoad="imageLoad" />
+      <detail-param-info :param-info="paramInfo" />
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from "./childComps/DetailNavBar.vue";
 import DetailSwiper from "./childComps/DetailSwiper.vue";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+import DetailGoodsInfo from "./childComps/DetailGoodsInfo.vue";
+import DetailParamInfo from "./childComps/DetailParamInfo.vue";
 
-import { getDetail } from "network/detail";
+import Scroll from "components/common/scroll/Scroll.vue";
+
+import { getDetail, Goods, Shop, GoodsParam } from "network/detail";
 
 export default {
-  name:'Detail',
+  name: "Detail",
   components: {
     DetailNavBar,
     DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    DetailGoodsInfo,
+    DetailParamInfo,
+    Scroll,
   },
   data() {
     return {
       iid: null,
-      topImages:[],
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {},
     };
   },
   created() {
@@ -28,15 +50,54 @@ export default {
     this.iid = this.$route.params.iid;
     // 2.根据iid请求详情数据
     getDetail(this.iid).then((res) => {
-      console.log(res)
+      console.log(res);
+      const data = res.result;
       // 1.获取顶部图片轮播数据
-      this.topImages = res.result.itemInfo.topImages;
-
+      this.topImages = data.itemInfo.topImages;
+      // 2.获取价格相关信息数据
+      this.goods = new Goods(
+        data.itemInfo,
+        data.columns,
+        data.shopInfo.services
+      );
+      // 3.获取店铺相关信息
+      this.shop = new Shop(data.shopInfo);
+      // 4.获取商品的详情数据
+      this.detailInfo = data.detailInfo;
+      // 5.获取参数信息
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
     });
   },
-  methods: {},
+  methods: {
+    imageLoad() {
+      this.$refs.scroll.refresh();
+    },
+  },
 };
 </script>
 
 <style>
+#detail {
+  position: relative;
+  z-index: 9999;
+  background-color: #fff;
+  height: 100vh;
+}
+.detail-nav {
+  position: relative;
+  z-index: 9999;
+  background-color: #fff;
+}
+.detail-content {
+  /* position: absolute;
+  top: 44px;
+  left: 0;
+  right: 0;
+  bottom: 0; */
+  height: calc(100% - 44px);
+  /* overflow: hidden; */
+}
 </style>
