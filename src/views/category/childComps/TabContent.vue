267eb5c404e54013a2de9e7c094ea6c544defda0
ZZK <1042134104@qq.com>
2021-01-22 14:00:38 +0800

    '分类和个人详情页完善'

---
+++ src/views/category/childComps/TabContent.vue
@@ -0,0 +1,35 @@
+<template>
+</template>
+
+<script>
+
+	import TabContentCategory from "./TabContentCategory";
+  import TabContentDetail from "./TabContentDetail";
+  import Scroll from "components/common/scroll/Scroll.vue";
+
+  export default {
+		name: "TabContent",
+    components: {
+		  Scroll,
+      TabContentCategory,
+      TabContentDetail
+    },
+    props: {
+      subcategories: {
+        type: Object,
+        default() {
+          return {}
+        }
+      },
+      categoryDetail: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+	}
+</script>
+
+<style scoped>
+</style>
